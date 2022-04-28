e812d3418fb4e96d2a5e471e3f8ca0e0d04d1ce8
Elder Pum <ElderPum@gmail.com>
2022-04-28 06:25:52 -0600

    Frontend terminado, faltan pruebas

---
+++ frontend/src/components/SideBarData.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FiGithub, FiMail, FiLinkedin } from 'react-icons/fi';
+export const SideBarData = [
+  {
+    title: 'https://github.com/elderpum',
+    direccion: 'https://github.com/elderpum',
+    icon: <FiGithub />,
+  },
+  {
+    title: '2823918350101@ingenieria.usac.edu.gt',
+    direccion:
+      'mailto:ElderPum@gmail.com?subject=Contacto%20de%20Trabajo',
+    icon: <FiMail />,
+  },
+];
